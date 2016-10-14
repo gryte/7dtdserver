2560092d0af3847a8f62943cbe981ef1cdb8b744
Adam Linkous <alinkous@gmail.com>
2016-10-13 21:18:16 -0500

    install steamcmd
    * add inspec tests
    * install steamcmd and supporting dependencies
    * citation: https://danielgibbs.co.uk/2014/02/steamcmd/
    * citation: https://steamcommunity.com/sharedfiles/filedetails/?id=360404397
    * citation: https://developer.valvesoftware.com/wiki/SteamCMD#Linux.2FOS_X

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -11,8 +11,19 @@ end
 # 7days user exists
 describe user('7days') do
   it { should exist }
+  its('group') { should eq '7days' }
   its('home') { should eq '/home/7days' }
   its('shell') { should eq '/bin/bash' }
+end
+
+# 7days user exists
+describe group('7days') do
+  it { should exist }
+end
+
+# /home/7days directory exists
+describe directory('/home/7days') do
+  it { should be_directory }
 end
 
 # screen is installed
@@ -20,17 +31,27 @@ describe package('screen') do
   it { should be_installed }
 end
 
-# steamcmd exists
+# steamcmd package exists
 describe file('/.downloads/steamcmd_linux.tar.gz') do
   it { should be_file }
 end
 
 # glibc.i686 is installed
-describe package('glibc.i686') do
-  it { should be_installed }
+describe command('yum list glibc.i686') do
+  its('stdout') { should match('glibc\.i686') }
 end
 
 # libstdc++.i686 is installed
-describe package('libstdc++.i686') do
-  it { should be_installed }
+describe command('yum list libstdc++.i686') do
+  its('stdout') { should match('libstdc\+\+\.i686') }
+end
+
+# steamcmd directory exists
+describe directory('/home/7days/steamcmd') do
+  it { should be_directory }
+end
+
+# steamcmd opened package exists
+describe file('/home/7days/steamcmd/steamcmd.sh') do
+  it { should be_file }
 end
